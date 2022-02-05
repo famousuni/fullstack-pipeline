a3902d8ef30d8a74b9c39f8bb79ac38c53271bd0
Charlie Gutierrez <36178883+famousuni@users.noreply.github.com>
2022-02-05 12:38:39 -0500

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 AWS_CONFIG_FILE=$HOME/.aws/config
 
-mkdir $HOME/.aws
-touch $AWS_CONFIG_FILE
-chmod 600 $AWS_CONFIG_FILE
+#mkdir $HOME/.aws
+#touch $AWS_CONFIG_FILE
+#chmod 600 $AWS_CONFIG_FILE
 
 echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
