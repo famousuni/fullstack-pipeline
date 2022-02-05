54dd922dd0e931194bbec23927be0d4ce1af2062
Charlie Gutierrez <36178883+famousuni@users.noreply.github.com>
2022-02-05 12:35:05 -0500

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,18 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+AWS_CONFIG_FILE=$HOME/.aws/config
+
+mkdir $HOME/.aws
+touch $AWS_CONFIG_FILE
+chmod 600 $AWS_CONFIG_FILE
+
+echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
+
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
+
+
 eb deploy
